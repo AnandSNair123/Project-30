e2505e17518bb8b23190431614a6541f5aaa40a0
AnandSNair123 <76837819+AnandSNair123@users.noreply.github.com>
2021-05-18 19:23:00 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,28 @@
+class Block{
+  constructor(x, y, width, height) {
+      var options = {
+          restitution :0.4,
+          friction :0.0,
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      this.Visiblity = 255;
+      World.add(world, this.body);
+      this.image = loadImage("block.png");
+    }
+    display(){
+      var pos= this.body.position;
+      imageMode(CENTER);
+      image(this.image, pos.x,pos.y, 30,40);
+      if(this.body.speed < 3){
+       }
+       else{
+         World.remove(world, this.body);
+         push();
+         this.Visiblity = this.Visiblity - 10;
+         tint(255,this.Visiblity);
+         pop();
+       }
+      }
+    }
